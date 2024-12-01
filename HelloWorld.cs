854df07efb87db8fe033501cec37178de53dd2fd
1fxsd <m02891979@gmail.com>
2024-12-01 15:10:08 +0300

    HelloWorld file

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class qw : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Вывод приветствия игроку
+        print("Hello, World!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
